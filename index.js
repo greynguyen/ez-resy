94e13f1bcd55f31a0f7bb020be2995f5c67bb9ba
Grey Nguyen <duongdangng@gmail.com>
2024-05-12 14:12:31 -0400

    Add scheduler

--- index.js
+++ index.js
@@ -3,25 +3,36 @@ import {
   getBookingConfig,
   makeBooking,
   fetchDataAndParseSlots,
-} from './utils/bookingLogic.js';
+} from "./utils/bookingLogic.js";
 
-import { checkTokenExpiration } from './utils/helpers.js';
+import { checkTokenExpiration } from "./utils/helpers.js";
+import schedule from "node-schedule";
 
-// Run the script
-let token = await checkTokenExpiration(process.env.AUTH_TOKEN);
-if (token) {
-  let existingBooking = await checkForExistingBooking();
-  if (!existingBooking) {
-    let slots = await fetchDataAndParseSlots();
+// Function to run booking process
+async function runBookingProcess() {
+  let token = await checkTokenExpiration(process.env.AUTH_TOKEN);
+  if (token) {
+    let existingBooking = await checkForExistingBooking();
+    if (!existingBooking) {
+      let slots = await fetchDataAndParseSlots();
 
-    if (slots) {
-      let bookToken = await getBookingConfig(slots);
-      let booking = await makeBooking(bookToken);
-      if (booking.resy_token) {
-        console.log(`You've got a reservation!`);
-      } else {
-        console.log(`Something went to 💩`);
+      if (slots) {
+        let bookToken = await getBookingConfig(slots);
+        let booking = await makeBooking(bookToken);
+        if (booking.resy_token) {
+          console.log(`You've got a reservation!`);
+        } else {
+          console.log(`Something went to 💩`);
+        }
       }
     }
   }
 }
+
+// Schedule the booking process to run every day at 10:00 AM
+const job = schedule.scheduleJob("*/5 * * * *", function () {
+  console.log("Attempting to run the booking process...");
+  runBookingProcess();
+});
+
+console.log("Booking job has been scheduled to run every 5 minutes.");
