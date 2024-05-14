4fa647369da27ff5b115ffcc5182f8a7e4a16558
Grey Nguyen <duongdangng@gmail.com>
2024-05-14 14:16:51 -0400

    Update code to refresh every second

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import schedule from "node-schedule";
 
 // Function to run booking process
 async function runBookingProcess() {
+  const now = getTime();
   let token = await checkTokenExpiration(process.env.AUTH_TOKEN);
   if (token) {
     let existingBooking = await checkForExistingBooking();
@@ -20,19 +21,41 @@ async function runBookingProcess() {
         let bookToken = await getBookingConfig(slots);
         let booking = await makeBooking(bookToken);
         if (booking.resy_token) {
-          console.log(`You've got a reservation!`);
+          console.log(`[${now}] – You've got a reservation!`);
         } else {
-          console.log(`Something went to 💩`);
+          console.log(`[${now}] – Something went to 💩`);
         }
       }
     }
   }
 }
 
-// Schedule the booking process to run every day at 10:00 AM
-const job = schedule.scheduleJob("*/5 * * * *", function () {
-  console.log("Attempting to run the booking process...");
-  runBookingProcess();
-});
+function getTime() {
+  const now = new Date().toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+  return now;
+}
 
-console.log("Booking job has been scheduled to run every 5 minutes.");
+// // Schedule the booking process to run every day at 10:00 AM
+// const job = schedule.scheduleJob("*/5 * * * *", function () {
+//   const now = getTime();
+//   console.log(`[${now}] – Attempting to run the booking process...`);
+//   runBookingProcess();
+// });
+
+// Schedule the booking process to run every second
+setInterval(() => {
+  const now = getTime();
+  console.log(`[${now}] – Attempting to run the booking process...`);
+  runBookingProcess();
+}, 1000);
+
+const now = getTime();
+
+console.log(
+  `[${now}] – Booking job has been scheduled to run every 5 minutes.`
+);
